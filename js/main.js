003300914529dc07af6b888ed3dcc7ef4dcd69a8
Canux <canux@Narvaez-Carlos.local>
2025-05-06 20:10:55 -0700

    Primer commit de demo-one

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// main.js
+
+console.log("Sandbox listo 👨‍💻");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector("h1");
+  heading.style.color = "#ffb547"; // Estilo dinámico para probar JS
+});
